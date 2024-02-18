e7c616e0206a327ca7127122457e6293f22fe029
itumandeev <itumandeev@vtb.ru>
2024-02-18 13:57:51 +0300

    add Lession1

---
+++ Lession1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void sayHello(){
+        System.out.println("Hello world!");
+    }
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
