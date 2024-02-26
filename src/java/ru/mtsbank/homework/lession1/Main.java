0902c457ae1f76255e5b323c08d66ea20e86f3e3
itumandeev <itumandeev@vtb.ru>
2024-02-26 15:21:51 +0300

    refactor

--- src/java/ru/mtsbank/homework/lession1/Main.java
+++ src/java/ru/mtsbank/homework/lession1/Main.java
@@ -1,8 +1,6 @@
-package org.example;
-
 public class Main {
     public static void sayHello(){
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
     }
     public static void main(String[] args) {
         sayHello();
