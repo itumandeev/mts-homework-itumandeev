49a8be5a5b1d3923b2ccffe739d2871a88d452f9
itumandeev <itumandeev@vtb.ru>
2024-02-26 15:38:26 +0300

    world change

--- src/java/ru/mtsbank/homework/lession2/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void doSomething(){
-        System.out.println("5 minutes");
-    }
-
-    public static void main(String[] args) {
-        doSomething();
-    }
-}
