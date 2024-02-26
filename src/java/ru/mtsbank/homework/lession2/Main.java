f341dda34ef763ca112283047f6a1e74d5dc71f6
itumandeev <itumandeev@vtb.ru>
2024-02-26 15:23:58 +0300

    refactor 2

---
+++ src/java/ru/mtsbank/homework/lession2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void doSomething(){
+        System.out.println("5 minutes");
+    }
+
+    public static void main(String[] args) {
+        doSomething();
+    }
+}
