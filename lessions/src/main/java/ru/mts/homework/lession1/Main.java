52f274befa9d9eaa08c9ece878f45b43d0c634ef
itumandeev <itumandeev@vtb.ru>
2024-02-26 15:39:39 +0300

    world change 2

---
+++ lessions/src/main/java/ru/mts/homework/lession1/Main.java
@@ -0,0 +1,10 @@
+package ru.mts.homework.lession1;
+
+public class Main {
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
---
+++ lessions/src/main/java/ru/mts/homework/lession2/Main.java
@@ -0,0 +1,7 @@
+package ru.mts.homework.lession2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Обучение верчение");
+    }
+}
