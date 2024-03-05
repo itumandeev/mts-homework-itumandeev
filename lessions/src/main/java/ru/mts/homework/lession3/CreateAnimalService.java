67601f4e26d89f900d1e47071b8636d79a44e98d
itumandeev <itumandeev@vtb.ru>
2024-03-05 16:05:03 +0300

    HW-1: исправление по замеяаниям

--- lessions/src/main/java/ru/mts/homework/lession3/CreateAnimalService.java
+++ lessions/src/main/java/ru/mts/homework/lession3/CreateAnimalService.java
@@ -1,19 +1,88 @@
 package ru.mts.homework.lession3;
 
-import java.util.Random;
-import java.util.UUID;
+import java.security.SecureRandom;
+import java.util.*;
+
+/**
+ * Интерфейс с методами
+ * -созданию эскземляра(10 экземпляров) нужно класса, на вход принимает String <b>type</b>
+ * -генерации случайных параметров: String <b>breed</> , String <b>name</>, String <b>character</>
+ *
+ * @author ITUMANDEEV
+ * version 0.2
+ */
 
 public interface CreateAnimalService {
-   default void create(){
+    default void createAnimals(String type) {
         int counter = 10;
-        while(counter!=0){
-            Cat cat = new Cat(
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    new Random().nextDouble());
-            System.out.println(cat);
+        System.out.println("Создаем животное " + type + " " + counter + " раз");
+        while (counter != 0) {
+            createAnimal(type);
             counter--;
         }
+    }
+
+    default void createAnimal(String type) {
+        switch (type) {
+            case ("cat"): {
+                Cat cat = new Cat(
+                        getRandomName(),
+                        getRandomBreed(),
+                        getRandomCharacter(),
+                        new Random().nextDouble());
+                System.out.println(cat);
+                break;
+            }
+            case ("dog"): {
+                Dog dog = new Dog(
+                        getRandomName(),
+                        getRandomBreed(),
+                        getRandomCharacter(),
+                        new Random().nextDouble());
+                System.out.println(dog);
+                break;
+            }
+            case ("shark"): {
+                Shark shark = new Shark(
+                        getRandomName(),
+                        getRandomBreed());
+                System.out.println(shark);
+                break;
+            }
+            case ("wolf"): {
+                Wolf wolf = new Wolf(
+                        getRandomName(),
+                        getRandomBreed());
+                System.out.println(wolf);
+                break;
+            }
+            default: {
+                System.out.println("Неизвестное животное");
+            }
+        }
+    }
+
+    default String getRandomName() {
+        Random random = new SecureRandom();
+        List<String> list = Arrays.asList("Вася", "Муся", "Пуся", "Дуся", "Куся", "Люся", "Вупсень", "Пупсень",
+                "Обиван", "Квайгон", "Арагорн", "Фродо", "Сэм", "Легосал", "Трор");
+        String name = list.get(random.nextInt(list.size()));
+        return name;
+    }
+
+    default String getRandomCharacter() {
+        Random random = new SecureRandom();
+        List<String> list = Arrays.asList("Спокойный", "Сказочный", "Мутный", "Шабутной", "Пришибленный", "Кантуженный", "Ласковый", "Добрый",
+                "Кремень", "Дикий");
+        String name = list.get(random.nextInt(list.size()));
+        return name;
+    }
+
+    default String getRandomBreed() {
+        Random random = new SecureRandom();
+        List<String> list = Arrays.asList("Лесной", "Степной", "Пустынный", "Капустный", "Деревянный", "Камышовый", "Домашний", "Какашный",
+                "Диванный", "Коренной");
+        String name = list.get(random.nextInt(list.size()));
+        return name;
     }
 }
--- lessions/src/main/java/ru/mts/homework/lession3/Main.java
+++ lessions/src/main/java/ru/mts/homework/lession3/Main.java
@@ -1,45 +1,12 @@
 package ru.mts.homework.lession3;
 
-import java.util.Random;
-import java.util.UUID;
-
 public class Main {
     public static void main(String[] args) {
         CreateAnimalServiceImpl test1 = new CreateAnimalServiceImpl();
-        test1.create();
+        test1.createAnimals("dog");
         System.out.println("  ");
-        test1.create(3);
+        test1.createAnimals(3, "wolf");
         System.out.println("  ");
-        test1.createWrapper();
-    }
-}
-
-class CreateAnimalServiceImpl implements CreateAnimalService{
-    public void create(int count) {
-        for(int i = count;i>0;i--){
-            Cat cat = new Cat(
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    new Random().nextDouble());
-            System.out.println(cat);
-        }
-    }
-
-    @Override
-    public void create() {
-        int counter = 5;
-        do{
-            Cat cat = new Cat(
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    UUID.randomUUID().toString(),
-                    new Random().nextDouble());
-            System.out.println(cat);
-            counter--;
-        } while (counter>0);
-    }
-    public void createWrapper(){
-        CreateAnimalService.super.create();
+        test1.createWrapper("cat");
     }
 }
